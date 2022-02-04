d64c43d8ea36d9788f11f2fce9b5c190d40faefb
Bia Dev <biacalepso9@gmail.com>
2022-02-03 21:42:22 -0300

    Api WhatsApp

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-var message = 'Hello World';
-console.log(message);
+"use strict";
+exports.__esModule = true;
+var whatsapp_web_js_1 = require("whatsapp-web.js");
+var qrcode = require('qrcode-terminal');
+var client = new whatsapp_web_js_1.Client({});
+client.on('qr', function (qr) {
+    console.log(qr);
+    qrcode.generate(qr, { small: true });
+});
+client.on('ready', function () {
+    console.log('ok');
+});
+client.on('message', function (msg) {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+    if (msg.body == 'oi') {
+        msg.reply('opa');
+    }
+});
+client.initialize();
--- index.ts
+++ index.ts
@@ -1,3 +1,28 @@
-let message: string = 'Hello World'
-console.log(message)
+import {Client} from 'whatsapp-web.js'
+var qrcode = require('qrcode-terminal')
+
+const client = new Client({
+})
+
+client.on('qr', qr => {
+    console.log(qr)
+    qrcode.generate(qr,{small:true})
+}) 
+
+client.on('ready', ( ) => {
+    console.log('ok')  
+}) 
+
+client.on('message', msg => {
+    if(msg.body == '!ping') {
+        msg.reply('pong')
+    }
+    
+    if(msg.body == 'oi') {
+        msg.reply('opa')
+    }    
+})
+
+client.initialize()
+
 
